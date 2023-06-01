b9c375c0dd9dfe2f5dfeba980124fcef3d9ff663
John9135 <palacioalexander5@gmail.com>
2023-06-01 15:26:42 -0500

    Create schema supplies and adherir id to model supplies

--- models/supplies.py
+++ models/supplies.py
@@ -5,6 +5,7 @@ class Supplies(Base):
 
     __tablename__ = "supplies"
 
+    id = Column(Integer, primary_key = True)
     supplier_id = Column(Integer,ForeignKey("supplier.id"))
     product_id = Column(Integer, ForeignKey("product.id"))
     purchase_price = Column(Float)
